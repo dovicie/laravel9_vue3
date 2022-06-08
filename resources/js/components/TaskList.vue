17ef6692c53c59a25623430221201c3a1d4fa708
dovicie <hyokoyama8810@gmail.com>
2022-06-09 08:28:48 +0900

    [clean] コメントアウト削除

--- resources/js/components/TaskList.vue
+++ resources/js/components/TaskList.vue
@@ -22,46 +22,6 @@ const toggleEditForm = () => {
             </thead>
             <tbody>
                 <tr v-for="(task, index) in tasks" :key="index">
-                    <!-- <td class="border px-4 py-2">
-                        <button
-                            class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-                        >
-                            Done
-                        </button>
-                    </td>
-                    <td v-if="isOpenEditForm" class="border px-4 py-2">
-                        <input
-                            class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-                            type="text"
-                            id="content"
-                            v-model="task.content"
-                        />
-                    </td>
-                    <td v-else class="border px-4 py-2">{{ task.content }}</td>
-                    <td v-if="isOpenEditForm" class="border px-4 py-2">
-                        <button
-                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                            @click="toggleEditForm"
-                        >
-                            更新
-                        </button>
-                    </td>
-                    <td v-else class="border px-4 py-2">
-                        <button
-                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                            @click="toggleEditForm"
-                        >
-                            編集
-                        </button>
-                    </td>
-                    <td class="border px-4 py-2">
-                        <button
-                            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                            @click="deleteTask(task.id)"
-                        >
-                            削除
-                        </button>
-                    </td> -->
                     <TaskItem :content="task.content" :id="task.id" />
                 </tr>
             </tbody>
